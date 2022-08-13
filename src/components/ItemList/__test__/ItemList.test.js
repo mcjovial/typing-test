9b01c067c36d902f613023359afc2f56c414aa1b
Emmanuel Elias <emmanuelelias455@gmail.com>
2022-08-14 00:58:25 +0100

    itemlist component test

---
+++ src/components/ItemList/__test__/ItemList.test.js
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom"
+import ItemList from '../ItemList';
+
+
+const MockItemList = ({ name, data, symble, ...rest }) => {
+  return (
+      <BrowserRouter>
+        <ItemList 
+          name={name}
+          data={data}
+        />
+      </BrowserRouter>
+  )
+}
+
+describe("AddInput", () => {
+  it('should contain span tag with correct text', () => {
+    render(
+      <MockItemList 
+        name='CPM'
+        data='50'
+      />
+    );
+    const smallElement = screen.getByText(/50/i);
+    expect(smallElement).toContainHTML('span');
+  });
+
+  it('should render correct text content', () => {
+    render(
+      <MockItemList 
+        name='CPM'
+        data='50'
+      />
+    ); 
+    const element = screen.getByText(/50/i);
+    expect(element).toHaveTextContent('50');
+      expect(element).toBeVisible();
+  });
+})
